a0e66efc06eb372fd7d89d8cbc1f0f5b724e4e7b
ranjt12103 <ranjitppatil10@gmail.com>
2025-11-18 23:28:03 +0530

    Frontend Update

--- Frontend/src/pages/Landing.jsx
+++ Frontend/src/pages/Landing.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import './Landing/landing.css'
+import Navbar from './Landing/Navbar'
+import Footer from './Landing/Footer'
 
 const Landing = () => {
   return (
-    <div>
-      Home Landing
+    <div className='overflow-x-hidden'>
+      <Navbar />
+          Home Landing
+      <Footer />
     </div>
   )
 }
---
+++ Frontend/src/pages/Landing/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './landing.css'
+import { NAV_LINKS } from './landing'
+
+const Navbar = () => {
+  return (
+    <>
+      <nav>
+        <div className="container">
+          logo
+        </div>
+      </nav>
+
+      <MobileSideBar>
+        
+      </MobileSideBar>
+    </>
+  )
+}
+
+export default Navbar
